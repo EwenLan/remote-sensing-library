0bd9443fd5282fab8b81f70a054eb5032d7b319a
Ewen Lan <ewen_lan@outlook.com>
2019-03-18 23:16:15 +0800

    Optimize the simulate anneal algorithm.

--- Executable Script/Exp 2/SimulateAnneal.m
+++ Executable Script/Exp 2/SimulateAnneal.m
@@ -1,19 +1,19 @@
 img1 = imread('san_1.bmp');
 img2 = imread('san_2.bmp');
 destimg = imread('san_gt.bmp');
-argsVector = [0.4, 0.99, 0.7, 0.9];
+argsVector = [0.4, 0.99, 0.7, 0.85];
 % argsVector Arguments Declaration:
 % (1): san_1/san_2 Low Pass Filter Radius.
 % (2): san_1 Gamma Transform b.
 % (3): san_2 Gamma Transform b.
 % (4): Differ Image Power Ratio. Lower: More white. Higher: Less white.
 limitTemp = 0.0001;
-lastMark = 999999999;
+lastMark = 99999;
 tringArgsVector = argsVector;
 dE = 0;
-temp = 120;
-alpha = 0.99;
-trainingStep = 0.03;
+temp = 9999999;
+alpha = 0.998;
+trainingStep = 0.001;
 trainingTimes = 0;
 while temp > limitTemp
     fimg1 = ImageFrequencyDomainLowPassFilter(img1, tringArgsVector(1));
@@ -24,22 +24,29 @@ while temp > limitTemp
     bdimg = AutoBinarizeImage(differImg, argsVector(4));
     % Judgement: Make the mark lower.
     mark = sum(sum(abs(bdimg - destimg)));
-    dE = mark - lastMark;
-    if dE < 0
+    dE = lastMark - mark;
+    if dE >= 0
         % Accept current result.
         argsVector = tringArgsVector;
         lastMark = mark;
     else
         rnd = rand(1);
-        if exp(dE/temp) > rnd && exp(dE/temp) < 1
+        if exp(dE/temp) > rnd %&& exp(dE/temp) < 1
             argsVector = tringArgsVector;
+            lastMark = mark;
         else
             tringArgsVector = argsVector;
         end
     end
     temp = temp*alpha;
-    vecSelection = floor(rand(1)*length(argsVector)) + 1;
-    tringArgsVector(vecSelection) = LimitedAdd(tringArgsVector(vecSelection), randn()*trainingStep, 0, 1);
+    vecSelection = floor(rand()*length(argsVector)) + 1;
+    adjustStep = randn()*trainingStep;
+    tringArgsVector(vecSelection) = LimitedAdd(tringArgsVector(vecSelection), adjustStep, 0, 1);
+%     if vecSelection == 4
+%         fprintf('Adjusted Differ Image Power Ratio. (%.4f)\n', adjustStep);
+%         disp(tringArgsVector);
+%     end
     trainingTimes = trainingTimes + 1;
 end
+close all;
 imshow(bdimg);
