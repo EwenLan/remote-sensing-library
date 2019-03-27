7be216e453af9f4d5d520e300942dbd2dedd962d
Ewen Lan <ewen_lan@outlook.com>
2019-03-27 21:36:43 +0800

    Start to write the report of experiment 4.

--- Executable Script/Exp 4/ReadAirportImages.m
+++ Executable Script/Exp 4/ReadAirportImages.m
@@ -12,7 +12,7 @@ colorset = {[0 0.4470 0.7410], [0.8500 0.3250 0.0980],...
     [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560],...
     [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330],...
     [0.6350 0.0780 0.1840]};
-fmat = moviein(trainingTimes);
+% fmat = moviein(trainingTimes);
 
 for i = 1:trainingTimes
     diffVecs = zeros([size(imgVecs), kNum]);
@@ -26,8 +26,8 @@ for i = 1:trainingTimes
     [minValue, minIndex] = min(imgVecNorm, [], 2);
     newkCenter = zeros([kNum, vecDims]);
     
-    kMeansClusteringPlot(imgVecs, minIndex, kCenter, kNum);
-    fmat(:, i) = getframe;
+%     kMeansClusteringPlot(imgVecs, minIndex, kCenter, kNum);
+%     fmat(:, i) = getframe;
     
     for j = 1:kNum
         newkCenter(j,:) = mean(imgVecs(minIndex == j,:));
@@ -39,16 +39,16 @@ for i = 1:trainingTimes
 %     fmat(:,i) = getframe;
 end
 
-% classifiedImgArea = reshape(minIndex, imgsize(1:2));
-% for i = 1:kNum
-%     figure;
-%     subplot(1, 2, 1);
-%     imshow(classifiedImgArea == i);
-%     subplot(1, 2, 2);
-%     simg = img.*uint8(classifiedImgArea == i);
-%     imshow(simg);
-% end
+classifiedImgArea = reshape(minIndex, imgsize(1:2));
+for i = 1:kNum
+    figure;
+    subplot(1, 2, 1);
+    imshow(classifiedImgArea == i);
+    subplot(1, 2, 2);
+    simg = img.*uint8(classifiedImgArea == i);
+    imshow(simg);
+end
 % figure;
-movie(fmat, 100, 10);
+% movie(fmat, 100, 10);
 
-% kMeansClusteringPlot(imgVecs, minIndex, kCenter, kNum);
+kMeansClusteringPlot(imgVecs, minIndex, kCenter, kNum);
