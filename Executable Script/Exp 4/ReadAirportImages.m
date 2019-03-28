4a569e2bc99d57961855ad65b48a6e842cbc3b96
Ewen Lan <ewen_lan@outlook.com>
2019-03-28 17:47:51 +0800

    March 28 2019 daily commit.

--- Executable Script/Exp 4/ReadAirportImages.m
+++ Executable Script/Exp 4/ReadAirportImages.m
@@ -1,20 +1,18 @@
 img = imread('airport_44.jpg');
 imgsize = size(img);
-%imshow(img);
 
 imgVecs = double(reshape(img, [], 3))/255;
 imgVecsSize = size(imgVecs);
 vecDims = imgVecsSize(2);
 kNum = 2;
 kCenter = rand(kNum, vecDims);
-trainingTimes = 20;
-colorset = {[0 0.4470 0.7410], [0.8500 0.3250 0.0980],...
-    [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560],...
-    [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330],...
-    [0.6350 0.0780 0.1840]};
-% fmat = moviein(trainingTimes);
+lastkCenter = rand(kNum, vecDims);
+maxTrainingTimes = 20;
+trainingTimes = 1;
 
-for i = 1:trainingTimes
+gifFrame = {};
+fig = figure;
+while ~isequal(kCenter, lastkCenter) && trainingTimes <= maxTrainingTimes
     diffVecs = zeros([size(imgVecs), kNum]);
     for j = 1:kNum
         diffVecs(:,:,j) = imgVecs - kCenter(j,:);
@@ -26,18 +24,19 @@ for i = 1:trainingTimes
     [minValue, minIndex] = min(imgVecNorm, [], 2);
     newkCenter = zeros([kNum, vecDims]);
     
-%     kMeansClusteringPlot(imgVecs, minIndex, kCenter, kNum);
-%     fmat(:, i) = getframe;
-    
+    kMeansClusteringPlot(imgVecs, minIndex, kCenter, kNum);
+    text(0.1, 0.9, 1, sprintf('Frame: %d', trainingTimes));
+    drawnow;
+    gifFrame{trainingTimes} = frame2im(getframe(fig));
+
     for j = 1:kNum
         newkCenter(j,:) = mean(imgVecs(minIndex == j,:));
     end
+    lastkCenter = kCenter;
     kCenter = newkCenter;
-    
-%     classifiedImgArea = reshape(minIndex, imgsize(1:2));
-%     imshow(classifiedImgArea == 1);
-%     fmat(:,i) = getframe;
+    trainingTimes = trainingTimes + 1;
 end
+close(fig);
 
 classifiedImgArea = reshape(minIndex, imgsize(1:2));
 for i = 1:kNum
@@ -47,8 +46,15 @@ for i = 1:kNum
     subplot(1, 2, 2);
     simg = img.*uint8(classifiedImgArea == i);
     imshow(simg);
+    imwrite(classifiedImgArea == i, sprintf('../../Document/figure/airport_44_Class_%02d_Logical.png', i));
+    imwrite(simg, sprintf('../../Document/figure/airport_44_Class_%02d_Separated.png', i));
 end
-% figure;
-% movie(fmat, 100, 10);
 
-kMeansClusteringPlot(imgVecs, minIndex, kCenter, kNum);
+for i = 1:trainingTimes - 1
+    [A, map] = rgb2ind(gifFrame{i}, 256);
+    if i == 1
+        imwrite(A, map, '../../Document/figure/airport_44_Clustering_Animation.gif', 'gif', 'LoopCount', Inf, 'DelayTime', 0.2);
+    else
+        imwrite(A, map, '../../Document/figure/airport_44_Clustering_Animation.gif', 'gif', 'WriteMode', 'append', 'DelayTime', 0.2);
+    end
+end
